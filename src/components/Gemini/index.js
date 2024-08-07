19d95496f8cf44662e0866271447e6a739a6e895
BadaBingBadaBo0m <alexbreathwitone@gmail.com>
2024-08-07 15:09:49 -0700

    gemini working

---
+++ src/components/Gemini/index.js
@@ -0,0 +1,60 @@
+"use client"
+
+import React, { useState } from "react"
+
+
+
+const Gemini = () => {
+  const [input, setInput] = useState("");
+  const [responseData, setResponseData] = useState("");
+
+  const {
+    GoogleGenerativeAI,
+    HarmCategory,
+    HarmBlockThreshold,
+  } = require("@google/generative-ai");
+
+  const apiKey = process.env.NEXT_PUBLIC_GEMINI_API_KEY;
+  const genAI = new GoogleGenerativeAI(apiKey);
+
+  const model = genAI.getGenerativeModel({
+    model: "gemini-1.5-flash",
+  });
+
+  const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 64,
+    maxOutputTokens: 8192,
+    responseMimeType: "text/plain",
+  };
+
+  async function run() {
+    const chatSession = model.startChat({
+      generationConfig,
+      // safetySettings: Adjust safety settings
+      // See https://ai.google.dev/gemini-api/docs/safety-settings
+      history: [
+      ],
+    });
+
+    const result = await chatSession.sendMessage(input);
+    console.log(result.response.text());
+    setResponseData(result.response.text());
+    setInput("");
+  }
+
+
+  return (
+    <div>
+      <h1>Gemini</h1>
+      <textarea className="text-black" readOnly rows={20} value={responseData} />
+
+      <textarea className="text-black" value={input} rows={3} onChange={(e) => setInput(e.target.value)} />
+
+      <button onClick={run}>Send Message</button>
+    </div>
+  )
+};
+
+export default Gemini;
