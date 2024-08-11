2a7cdfaf55b53f86466fe6437a040e87d5253f04
BadaBingBadaBo0m <alexbreathwitone@gmail.com>
2024-08-11 13:31:36 -0700

    custom scroll bar

---
+++ src/components/OpenModalButton/index.js
@@ -0,0 +1,29 @@
+"use client"
+
+import React, { lazy, Suspense } from 'react';
+import { useModal } from '@/context/Modal';
+
+function OpenModalButton({
+  modalComponent, // component to render inside the modal
+  buttonText, // text of the button that opens the modal
+  onButtonClick, // optional: callback function that will be called once the button that opens the modal is clicked
+  onModalClose // optional: callback function that will be called once the modal is closed
+}) {
+  const { setModalContent, setOnModalClose } = useModal();
+
+  const onClick = () => {
+    if (onModalClose) setOnModalClose(onModalClose);
+    setModalContent(
+      <Suspense fallback={<div>Loading...</div>}>
+        <modalComponent />
+      </Suspense>
+    );
+    if (onButtonClick) onButtonClick();
+  };
+
+  return (
+    <button onClick={() => onClick()}>{buttonText}</button>
+  );
+}
+
+export default OpenModalButton;
